ac928b7ed57366a0d1a3a5fe1714b35683f3c31e
fefi95 <scct95@gmail.com>
2018-09-05 11:21:19 -0500

    Added holidays (1)

--- js/base.js
+++ js/base.js
@@ -1,3 +1,14 @@
+var holidays = [
+  { month: 1 , day: 1 },
+  { month: 1 , day: 15 },
+  { month: 4, day: 19 },
+  { month: 5, day: 24 },
+  { month: 6, day: 24 },
+  { month: 7, day: 5 },
+  { month: 10, day: 12 },
+  { month: 12, day: 25 },
+]
+
 $( function() {
 
   // Add afterShow function to the jQuery's datepicker
@@ -31,6 +42,18 @@ $( function() {
       let months = maxD.getMonth() - minD.getMonth() + 1;
       return { minDate: minD, maxDate: maxD, numberOfMonths: months }
     },
+    beforeShowDay: function (date) {
+        var date2 = {
+          month: date.getMonth() + 1,
+          day: date.getDate()
+        }
+        if ( isHoliday( date2 ) ) {
+          return [true, "ui-datepicker-unselectable ui-datepicker-holiday"];
+        }
+        else {
+          return [true, ""];
+        }
+    },
     afterShow: function(inst)  {
       // Delete extra rows to the calendar
       let daysInWeek = 7
@@ -55,4 +78,20 @@ $( function() {
 */
 function addDays(date, days) {
   return new Date(date.getTime() + days*24*60*60*1000);
+}
+
+function isHoliday( date ) {
+  let isHoliday = false;
+  console.log(date);
+  for (var i in holidays) {
+    isHoliday = compareDates(holidays[i], date );
+    console.log(holidays[i]);
+    if( isHoliday ){
+      return isHoliday;
+    }
+  }
+  return false;
+}
+function compareDates( date1, date2 ) {
+  return date1.month === date2.month && date1.day === date2.day;
 }
